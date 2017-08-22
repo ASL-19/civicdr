ddbff7c2030be906fa8d49b6032f34fef60a9c34
seamus tuohy <code@seamustuohy.com>
2017-08-22 09:48:20 -0400

    Trying as anonymous functions

--- app/assets/scripts/views/provider-form.js
+++ app/assets/scripts/views/provider-form.js
@@ -289,7 +289,7 @@ var ProviderForm = React.createClass({
               {!isAdmin
                 ? <div className='form__group checkboxes-light form__group--large'>
                   <label className='form__label-dark'>Code of Practice</label>
-                  <p className='form__help'>You must read and agree to the terms of CiviCDRs <a href="#" onClick={this.props.dispatch(displayCodeOfPractice)}>Code of Practice</a> to create an account on this platform.</p>
+                  <p className='form__help'>You must read and agree to the terms of CiviCDRs <a href="#" onClick={() => this.props.dispatch(displayCodeOfPractice())}>Code of Practice</a> to create an account on this platform.</p>
                   <label className='form__option form__option--custom-checkbox' key='code-of-conduct'>
                     <input
                       type='checkbox'
@@ -297,7 +297,7 @@ var ProviderForm = React.createClass({
                       required={true}
                       value='agree_cop'
                     />
-                    <span className='form__option__text'>I have read and agree to the <a href="#" onClick={this.props.dispatch(displayCodeOfPractice)}>Code of Practice.</a></span>
+               <span className='form__option__text'>I have read and agree to the <a href="#" onClick={() => this.props.dispatch(displayCodeOfPractice())}>Code of Practice.</a></span>
                     <span className='form__option__ui'></span>
                   </label>
                 </div>
